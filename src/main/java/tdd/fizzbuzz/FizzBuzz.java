c493b74ed22b0477f78c1dc8f9c2a2d4b2ee761b
maruri-git <peluema.github@gmail.com>
2022-09-01 14:34:27 +0800

    refactor: implement return whizz function

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,12 +3,16 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
     public String FIZZ = "Fizz";
     public String BUZZ = "Buzz";
+    public String WHIZZ = "Whizz";
     public String countOff(int input) {
         if(input % 3 == 0) {
             return FIZZ;
         }
         if(input % 5 == 0) {
             return BUZZ;
+        }
+        if(input % 7 == 0) {
+            return WHIZZ;
         }
         return String.valueOf(input);
     }
